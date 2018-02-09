e0dfcf9621ffb522ab8c8b5c2f14652cfa6bc5dd
Ruslan Ibragimov <ruslan@ibragimov.by>
2018-02-09 16:33:28 +0300

    Declare module dependencies bootique/bootique#203

--- src/main/java/io/bootique/liquibase/LiquibaseModuleProvider.java
+++ src/main/java/io/bootique/liquibase/LiquibaseModuleProvider.java
@@ -3,29 +3,38 @@ package io.bootique.liquibase;
 import com.google.inject.Module;
 import io.bootique.BQModule;
 import io.bootique.BQModuleProvider;
+import io.bootique.jdbc.JdbcModuleProvider;
 
 import java.lang.reflect.Type;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.Map;
 
 public class LiquibaseModuleProvider implements BQModuleProvider {
 
-	@Override
-	public Module module() {
-		return new LiquibaseModule();
-	}
+    @Override
+    public Module module() {
+        return new LiquibaseModule();
+    }
 
-	@Override
-	public Map<String, Type> configs() {
-		// TODO: config prefix is hardcoded. Refactor away from ConfigModule, and make provider
-		// generate config prefix, reusing it in metadata...
-		return Collections.singletonMap("liquibase", LiquibaseFactory.class);
-	}
+    @Override
+    public Map<String, Type> configs() {
+        // TODO: config prefix is hardcoded. Refactor away from ConfigModule, and make provider
+        // generate config prefix, reusing it in metadata...
+        return Collections.singletonMap("liquibase", LiquibaseFactory.class);
+    }
 
-	@Override
-	public BQModule.Builder moduleBuilder() {
-		return BQModuleProvider.super
-				.moduleBuilder()
-				.description("Provides integration with Liquibase SQL migrations framework.");
-	}
+    @Override
+    public BQModule.Builder moduleBuilder() {
+        return BQModuleProvider.super
+                .moduleBuilder()
+                .description("Provides integration with Liquibase SQL migrations framework.");
+    }
+
+    @Override
+    public Collection<BQModuleProvider> dependencies() {
+        return Collections.singletonList(
+                new JdbcModuleProvider()
+        );
+    }
 }
--- src/test/java/io/bootique/liquibase/LiquibaseModuleProviderTest.java
+++ src/test/java/io/bootique/liquibase/LiquibaseModuleProviderTest.java
@@ -1,17 +1,35 @@
 package io.bootique.liquibase;
 
+import io.bootique.BQRuntime;
+import io.bootique.jdbc.JdbcModule;
 import io.bootique.test.junit.BQModuleProviderChecker;
+import io.bootique.test.junit.BQTestFactory;
+import org.junit.Rule;
 import org.junit.Test;
+
+import static com.google.common.collect.ImmutableList.of;
 
 public class LiquibaseModuleProviderTest {
 
-	@Test
-	public void testPresentInJar() {
-		BQModuleProviderChecker.testPresentInJar(LiquibaseModuleProvider.class);
-	}
+    @Rule
+    public BQTestFactory testFactory = new BQTestFactory();
 
-	@Test
-	public void testMetadata() {
-		BQModuleProviderChecker.testMetadata(LiquibaseModuleProvider.class);
-	}
+    @Test
+    public void testPresentInJar() {
+        BQModuleProviderChecker.testPresentInJar(LiquibaseModuleProvider.class);
+    }
+
+    @Test
+    public void testMetadata() {
+        BQModuleProviderChecker.testMetadata(LiquibaseModuleProvider.class);
+    }
+
+    @Test
+    public void testModuleDeclaresDependencies() {
+        final BQRuntime bqRuntime = testFactory.app().module(new LiquibaseModuleProvider()).createRuntime();
+        BQModuleProviderChecker.testModulesLoaded(bqRuntime, of(
+                JdbcModule.class,
+                LiquibaseModule.class
+        ));
+    }
 }
